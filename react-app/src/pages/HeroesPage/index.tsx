0e169fa56eb3903f93d2ac57cb0efbfe89efe740
Anastasia <anast@example.com>
2026-05-21 18:06:33 +0300

    переніс інтерейс

--- react-app/src/pages/HeroesPage/index.tsx
+++ react-app/src/pages/HeroesPage/index.tsx
@@ -1,25 +1,5 @@
 import React, { useState, useEffect } from 'react';
-
-interface OpenDotaHero {
-  id: number;
-  name: string;
-  localized_name: string;
-  primary_attr: string;
-  roles: string[];
-  pro_win?: number;
-  pro_pick?: number;
-  base_health: number;
-  base_mana: number;
-  base_armor: number;
-  base_attack_min: number;
-  base_attack_max: number;
-  base_str: number;
-  base_agi: number;
-  base_int: number;
-  move_speed: number;
-  attack_range: number;
-}
-
+import type{OpenDotaHero} from '../../entityes/heroes/model/Iheroes';
 export const HeroesPage: React.FC = () => {
   const [heroes, setHeroes] = useState<OpenDotaHero[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
